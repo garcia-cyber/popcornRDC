cdd730af8a1b338d67ca7b5916bbe530037ad5ab
mukoko <mukoko@gmail.com>
2025-12-16 11:11:59 +0100

    pres

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# Cette ligne indique à Django quel fichier de paramètres (settings.py) utiliser.
+# 'conf.settings' pointe vers le fichier settings.py dans le dossier 'conf'.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 
+# Cette variable 'application' est le point d'entrée que le serveur de production
+# (comme Gunicorn) utilise pour lancer votre application Django.
 application = get_wsgi_application()
